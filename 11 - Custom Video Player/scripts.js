8ec4dcd8f0a7df895bd9b37032a5a96cd180de93
kxwzhang <kxwzhang@gmail.com>
2020-10-05 18:26:09 -0700

    Write togglePlay

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,7 +1,19 @@
 // Get our elements
 const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
 // Build our functions
-
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
 // Hook up event listeners
