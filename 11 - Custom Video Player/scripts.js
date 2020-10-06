0a5cd1bbeb754c3419964a94934881af71021602
kxwzhang <kxwzhang@gmail.com>
2020-10-05 18:33:57 -0700

    Hook up skipping

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -21,9 +21,15 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+
+}
+
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
