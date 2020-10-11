51c0dee1375d6392cd974c008cd0ddadf53902b2
kxwzhang <kxwzhang@gmail.com>
2020-10-11 15:13:03 -0700

    Paint to the canvas using ctx and setting and interval to every 16ms

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -18,7 +18,11 @@ function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
   canvas.width = width;
-  canvas.height = height;g
-}
+  canvas.height = height;
+  
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}g
 
 getVideo();
