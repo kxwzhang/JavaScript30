a632da014bceb2820a5fa6e8771268b971a6fde4
kxwzhang <kxwzhang@gmail.com>
2020-10-24 15:00:59 -0700

    Add helper function for displaying the first second of countdown

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,9 +1,22 @@
+let countdown;
+
 function timer(seconds) {
   const now = Date.now();
   const then = now + (seconds * 1000);
-  console.log({now, then});
-  setInterval(() => {
-    const secondsLeft = (then - Date.now()) / 1000;
-    console.log({secondsLeft})
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    // check if need to stop
+    if (secondsleft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    
+    displayTimeLeft(secondsLeft);
   }, 1000);
+};
+
+function displayTimeLeft(seconds) {
+  console.log(seconds);
 }
