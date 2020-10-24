316807934e25ea717dce965bdbdbc10e57f05722
kxwzhang <kxwzhang@gmail.com>
2020-10-24 14:56:54 -0700

    Run setInterval every second

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,3 +1,9 @@
 function timer(seconds) {
-  
+  const now = Date.now();
+  const then = now + (seconds * 1000);
+  console.log({now, then});
+  setInterval(() => {
+    const secondsLeft = (then - Date.now()) / 1000;
+    console.log({secondsLeft})
+  }, 1000);
 }
