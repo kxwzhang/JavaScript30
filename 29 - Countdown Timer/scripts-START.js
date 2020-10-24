955a4ad7358e388b7b6bc30aad65edd5ed630443
kxwzhang <kxwzhang@gmail.com>
2020-10-24 15:06:43 -0700

    Add 0 if remainding seconds left is < 10

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,4 +1,5 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds) {
   const now = Date.now();
@@ -8,15 +9,18 @@ function timer(seconds) {
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
     // check if need to stop
-    if (secondsleft < 0) {
+    if (secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-    
+
     displayTimeLeft(secondsLeft);
   }, 1000);
 };
 
 function displayTimeLeft(seconds) {
-  console.log(seconds);
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  const display = `${minutes}:${remainderSeconds < 10 ? 0 : ''}${remainderSeconds}`;
+  timerDisplay.textContent = display;
 }
