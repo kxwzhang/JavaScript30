cf54b35945143ec183fa53e436a967675332f7d1
kxwzhang <kxwzhang@gmail.com>
2020-10-24 15:15:16 -0700

    Adjust for 12hr interval

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,13 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + (seconds * 1000);
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -22,5 +25,13 @@ function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
   const display = `${minutes}:${remainderSeconds < 10 ? 0 : ''}${remainderSeconds}`;
+  document.title = display;
   timerDisplay.textContent = display;
-}
+};
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : 12}:${minutes < 10 ? 0 : ''}${minutes}`;
+};
